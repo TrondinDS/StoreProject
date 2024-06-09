8d66dd4c2f75d6e22df20b0a753e33bf4cf43382
TrondinDS <trondin000@gmail.com>
2024-06-09 17:47:05 +0300

    Add ApplicationContext, Add InterceptorOverridDelete

--- StoreProject/DB/Context/ApplicationContext.cs
+++ StoreProject/DB/Context/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StoreProject.DB.Interceptors;
 using StoreProject.DB.Models;
 
 namespace StoreProject.DB.Context
@@ -17,5 +18,19 @@ namespace StoreProject.DB.Context
         public DbSet<ProductType> productsType { get; set; }
         public DbSet<ProductInformation> productInformation { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(new InterceptorOverrideDelete());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasQueryFilter(x => x.IsDeleted == false);
+            modelBuilder.Entity<Order>().HasQueryFilter(x => x.IsDeleted == false);
+            modelBuilder.Entity<OrderItem>().HasQueryFilter(x => x.IsDeleted == false);
+            modelBuilder.Entity<ProductType>().HasQueryFilter(x => x.IsDeleted == false);
+            modelBuilder.Entity<Product>().HasQueryFilter(x => x.IsDeleted == false);
+            modelBuilder.Entity<ProductInformation>().HasQueryFilter(x => x.IsDeleted == false);
+        }
     }
 }
---
+++ StoreProject/DB/Interceptors/InterceptorOverrideDelete.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using StoreProject.DB.Models.Interfaces;
+
+namespace StoreProject.DB.Interceptors
+{
+    public class InterceptorOverrideDelete : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData dbEvent, InterceptionResult<int> result)
+        {
+            if (dbEvent.Context is null) return result;
+
+            foreach (var item in dbEvent.Context.ChangeTracker.Entries())
+            {
+                if (item is not { State: EntityState.Deleted, Entity: IsDelete delete }) continue;
+                item.State = EntityState.Modified;
+                delete.IsDeleted = true;
+            }
+            return result;
+        }
+
+        public async override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData dbEvent, InterceptionResult<int> result, CancellationToken token = default)
+        {
+            if (dbEvent.Context is null) return await  Task.FromResult(result);
+
+            foreach (var item in dbEvent.Context.ChangeTracker.Entries())
+            {
+                if (item is not { State: EntityState.Deleted, Entity: IsDelete delete }) continue;
+                item.State = EntityState.Modified;
+                delete.IsDeleted = true;
+            }
+            return await Task.FromResult(result);
+        }
+    }
+}
